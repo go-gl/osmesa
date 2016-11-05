db25c34a90ce93d5af074f57c312a745910927c9
crvv <crvv.pku@gmail.com>
2016-11-05 14:51:13 +0800

    use package github.com/go-gl/osmesa/gl

--- osmesa.go
+++ osmesa.go
@@ -51,7 +51,7 @@ package osmesa
 // #cgo pkg-config: osmesa
 // #include <GL/osmesa.h>
 import "C"
-import "github.com/go-gl/gl"
+import "github.com/go-gl/osmesa/gl"
 import "unsafe"
 
 type PixelType int
